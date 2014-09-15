601ebfa2a66b54a1a1f78b7018324156f14494eb
Ruben Rhodes & Catherine Tran <student@epicodus.com>
2014-09-15 15:18:51 -0700

    create show, edit, and updatecontrollers and htmls.

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -7,13 +7,28 @@ class StationsController < ApplicationController
     @station = Station.new
   end
 
+  def show
+    @station = Station.find(params[:id])
+  end
+
   def create
     @station = Station.create(station_params)
     redirect_to stations_path
   end
 
+  def edit
+    @station = Station.find(params[:id])
+  end
+
+  def update
+    @station = Station.find(params[:id])
+    @station.update(station_params)
+    flash[:notice] = "Your Station has been updated!"
+    redirect_to station_path(@station)
+  end
+
   private
   def station_params
-    params.require(:station).permit(:name)
+    params.require(:station).permit(:name, :id)
   end
 end
