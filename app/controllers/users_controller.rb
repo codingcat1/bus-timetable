a0e7d5e902ac0180bfb292bc21180cf02f705044
Ruben Rhodes & Catherine Tran <student@epicodus.com>
2014-09-16 14:46:13 -0700

    modify UserController create action and setting the user_id session variable when the new user is saved

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Thank you for signing up!"
       redirect_to root_url
     else
