9ff6a7034732f44ed36c0e7405ff93068ebeb38f
Ruben Rhodes & Catherine Tran <student@epicodus.com>
2014-09-16 16:50:09 -0700

    fixed specs for creating new station for admin and non admin

--- spec/features/create_new_station_spec.rb
+++ spec/features/create_new_station_spec.rb
@@ -5,13 +5,13 @@ describe "creating a new station" do
   it "allows an admin to create a new station" do
     user = User.create(:name => 'bob', :admin => true, :password => "123")
     sign_in(user)
-    expect(page).to have_content "Train information"
+    expect(page).to have_content "Create a new Station"
   end
 
   it "stops a non admin from creating a new station" do
     user = User.create(:name => 'bob', :admin => false, :password => "123")
     sign_in(user)
-    expect(page).to have_content "Train information"
+    expect(page).to have_content "Not authorized"
   end
 
 #   it "returns an error message when a station name is not provided" do
--- spec/utilities.rb
+++ spec/utilities.rb
@@ -3,4 +3,6 @@ def sign_in(user)
   fill_in 'name', :with => user.name
   fill_in 'password', :with => user.password
   click_button "Log In"
+  expect(page).to have_content "Train information"
+  click_link "New Station"
 end
